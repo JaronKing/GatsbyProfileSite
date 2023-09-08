45b4a0e51e520a07454688bb9954561d2896eeee
JaronKing <jaronjking@gmail.com>
2023-09-08 05:04:19 -0700

    Animated background with spinning square transform to circles.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,11 +8,57 @@ const Layout = ({ children }) => {
         <div>
             <Header/>
             <main className="flex p-6 min-h-screen
-                bg-gradient-to-r from-orange-400 to-orange-250">
+                context">
                 { children }
             </main>
+            <div className="area" >
+                <ul className="circles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+            </div >
             <Footer/>
         </div>
+
     )
 }
 
